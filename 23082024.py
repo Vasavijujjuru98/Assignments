db225487157c90db3c1bc47b715ea366d4a9c3c9
vasavi <vasavijujjuru98@gmail.com>
2024-08-26 16:06:39 +0530

    day3 assignment

---
+++ 23082024.py
@@ -0,0 +1,16 @@
+banking=input("enter your option") 
+if(banking == "banking"):
+   print(" show some option")
+elif(banking == "minstatement"):
+   print("show transaction history")
+elif(banking == "balance enquary"):
+   print("show the balance amount")
+elif(banking == "pingeneration"):
+   print("generate the pin")
+elif(banking == "withdrawal"):
+   print("deduct some amount from actual amount")
+else:
+   print("enter valid option")
+     
+
+    
