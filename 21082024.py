55770d80f3ee43cb062749d64c63b083f654988c
vasavi <vasavijujjuru98@gmail.com>
2024-08-26 15:39:36 +0530

    day1 assignment

---
+++ 21082024.py
@@ -0,0 +1,56 @@
+# this is day1 task
+print("hello world")
+num=20 # the value 20 is stored in num variable
+"""variable is a data container.
+   it is used to store the values or data 
+   variables. 
+   we donot need to declare type of variable before, directly assign value to the variable
+   because python is a dynamic typed language
+"""
+""" identifier is  the name we give to identify a variable,class , function, and other objects.
+To  declare a identifier we need to follow some rules such as
+1.identifiers are created using letters,digits and some special charcaters(_,$)
+1.both uppercase and lowercase letters are used in identifiers 
+2.keywords are not used as identifer.
+4.identifiers does not start with digits.
+5 spaces are not allowed between the names.
+"""
+name="vasavi"
+print(name)
+name_1="lakshmi"
+print(name_1)
+_name1="sai"
+print(_name1)
+#123name="srinu" this is not valid
+"""Data types in python is refers to the type of value a variable has and what type of operation like mathamatical
+logical and relation can be applied without causing an error.python having some built in data types such as
+integer
+float
+string
+boolean
+complex
+list
+tuple
+set
+dictionary
+"""
+num=20 # interger is a  data type to store numerical values
+print(type(num))# by using type function to find type of a variable
+print(id(num))#by using id function to find where the value to be stored
+name2="vasavi"#A group of characters enclosed with single quotes and double quotes is called string
+x=20.34567# float is a data type to declare a values like decimal points
+y=True #boolean is adata type it stores the values like True or False
+n=20+30j# complex is one of the data type its store the value like a+bj format
+print(name2)
+print(x)
+print(type(x))
+print(id(x))
+print(y)
+print(type(y))
+print(id(y))
+print(n)
+print(type(n))
+print(id(n))
+
+
+
